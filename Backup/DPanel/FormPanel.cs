0fb8ad7e1cc31ccf1ef63abe9d0415910941b838
Banee Ishaque K <Baneeishaque@gmail.com>
2018-12-07 14:17:26 +0530

    Backups

---
+++ Backup/DPanel/FormPanel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DPanel
+{
+    class FormPanel : Panel 
+    { bool previous; 
+        FormPanel l; 
+        public FormPanel() 
+        { previous = false; 
+            l.Parent = this; 
+            l.Dock = DockStyle.Fill; 
+        } 
+    }
+}
